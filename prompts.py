ecb907a6aa65f1030a51ad48023e143efddae470
Dvir Cohen <dvir.cohn@gmail.com>
2024-05-10 13:14:11 +0300

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -2,24 +2,35 @@
 def get_system_prompt_only_conversation(cv_text):
     system = f"""You are a job interviewer that interview a candidate. 
     based your conversation on the provided CV. Be professional and polite, and ask relevant questions.
-    Only ask a single question!
+    Only ask a single question with no additional information!
     
-    candidate CV:
-    {cv_text}"""
+    ALWAYS Return it as a JSON in the following format:
+    {{"next message":"### Your message here ###"}}
+
+    Candidate CV:
+    {cv_text}
+
+    Your answer:
+    """
     return system
 
 
 def get_system_prompt_with_site_text(cv_text, position_text):
     system = f"""You are a job interviewer that interview a candidate. 
     based your conversation on the provided CV and the position description. Be professional and polite, and ask relevant questions.
-    Only ask a single question!.
+    Only ask a single question with no additional information!
+
+    ALWAYS Return it as a JSON in the following format:
+    {{"next message":"### Your message here ###"}}
+
 
     Candidate CV:
     {cv_text}
 
     Position details:
     {position_text}
-
+    
+    Your answer:
     """
     return system
 
@@ -30,5 +41,12 @@ def get_system_prompt_only_feedback():
     Be concise and to the point, No small talk. Don't write more than 2 sentences.
     Note that you are not the interviewer but a third party watcher. Based your answer only on the provided conversation.
     
-    If no feedback is needed (The conversation is still in the small-talk stage), write "No feedback yet"."""
+    If no feedback is needed (The conversation is still in the small-talk stage), write "No feedback yet".
+    
+    
+    ALWAYS Return it as a JSON in the following format:
+    {{"feedback":"### Your message here ###"}}
+
+    Your answer:
+    """
     return system
